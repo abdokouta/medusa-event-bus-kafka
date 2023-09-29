ffabc8d332021a574110749e0fc755d182530893
Abdelrhman Kouta <pixiedia@gmail.com>
2023-09-29 04:28:20 +0300

    removing extra redis code

--- dist/services/redis.service.d.ts
+++ dist/services/redis.service.d.ts
@@ -1,4 +1,3 @@
-import { Observable } from "rxjs";
 import { EmitMessageResponse } from "../types";
 /**
  * Initializes the Redis service. 🚀
@@ -27,58 +26,4 @@ export declare class RedisService {
      * @returns {Promise<{ status: string, message: string }>} An object with status and message.
      */
     emitMessage(topic: any, message: any): Promise<EmitMessageResponse>;
-    /**
-     * Sets a key-value pair in Redis.
-     *
-     * @param key - The key to set.
-     * @param value - The value to set for the key.
-     * @param expiresIn - Optional expiration time in seconds.
-     * @returns A Promise resolving to "OK" on successful set.
-     */
-    set(key: string, value: string, expiresIn?: number): Promise<string>;
-    /**
-     * Gets the value associated with a key from Redis.
-     *
-     * @param key - The key to retrieve the value for.
-     * @returns A Promise resolving to the value for the given key, or null if the key is not found.
-     */
-    get(key: string): Promise<string | null>;
-    /**
-     * Deletes a key from Redis.
-     *
-     * @param key - The key to delete.
-     * @returns A Promise resolving to the number of keys deleted.
-     */
-    del(key: string): Promise<number>;
-    /**
-     * Subscribes to a Redis topic and returns an Observable to receive messages.
-     *
-     * @param topic - The topic to subscribe to.
-     * @returns An Observable emitting messages received on the topic.
-     */
-    subscribe(topic: string): Observable<string>;
-    /**
-     * Pings the Redis server to check if it's reachable.
-     *
-     * @returns A Promise resolving to the "PONG" response.
-     */
-    ping(): Promise<string>;
-    /**
-     * Subscribes to a Redis topic.
-     *
-     * @param topic - The topic to subscribe to.
-     */
-    subscribeToTopic(topic: string): void;
-    /**
-     * Unsubscribes from a Redis topic.
-     *
-     * @param topic - The topic to unsubscribe from.
-     */
-    unsubscribeFromTopic(topic: string): void;
-    /**
-     * Starts a Redis transaction.
-     *
-     * @returns A Promise resolving when the transaction is started.
-     */
-    startTransaction(): Promise<void>;
 }
--- dist/services/redis.service.js
+++ dist/services/redis.service.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RedisService = void 0;
 const ioredis_1 = require("ioredis");
-const rxjs_1 = require("rxjs");
 const uuid_1 = require("uuid");
 const config_1 = require("../config");
 /**
@@ -79,103 +78,6 @@ class RedisService {
             // Return error status and message
             return { status: "error", message: "Failed to send Redis event." };
         }
-    }
-    /**
-     * Sets a key-value pair in Redis.
-     *
-     * @param key - The key to set.
-     * @param value - The value to set for the key.
-     * @param expiresIn - Optional expiration time in seconds.
-     * @returns A Promise resolving to "OK" on successful set.
-     */
-    async set(key, value, expiresIn) {
-        const result = await this.redisClient.set(key, value);
-        // Set cache expiry if expiresIn is provided
-        if (expiresIn) {
-            await this.redisClient.expire(key, expiresIn);
-        }
-        return result;
-    }
-    /**
-     * Gets the value associated with a key from Redis.
-     *
-     * @param key - The key to retrieve the value for.
-     * @returns A Promise resolving to the value for the given key, or null if the key is not found.
-     */
-    async get(key) {
-        const result = await this.redisClient.get(key);
-        return result;
-    }
-    /**
-     * Deletes a key from Redis.
-     *
-     * @param key - The key to delete.
-     * @returns A Promise resolving to the number of keys deleted.
-     */
-    async del(key) {
-        const result = await this.redisClient.del(key);
-        return result;
-    }
-    /**
-     * Subscribes to a Redis topic and returns an Observable to receive messages.
-     *
-     * @param topic - The topic to subscribe to.
-     * @returns An Observable emitting messages received on the topic.
-     */
-    subscribe(topic) {
-        return new rxjs_1.Observable((subscriber) => {
-            this.redisClient.subscribe(topic, async (err, count) => {
-                if (err) {
-                    subscriber.error(err);
-                }
-                else {
-                    this.logger.info(`Subscribed to ${count} topic(s)`);
-                }
-            });
-            const messageSubscriber = (topic, message) => {
-                if (topic === topic) {
-                    subscriber.next(message);
-                }
-            };
-            this.redisClient.on("message", messageSubscriber);
-            return async () => {
-                this.logger.info(`Unsubscribed from ${topic}`);
-                this.redisClient.unsubscribe(topic);
-                this.redisClient.removeListener("message", messageSubscriber);
-            };
-        });
-    }
-    /**
-     * Pings the Redis server to check if it's reachable.
-     *
-     * @returns A Promise resolving to the "PONG" response.
-     */
-    async ping() {
-        return this.redisClient.ping();
-    }
-    /**
-     * Subscribes to a Redis topic.
-     *
-     * @param topic - The topic to subscribe to.
-     */
-    subscribeToTopic(topic) {
-        this.redisClient.subscribe(topic);
-    }
-    /**
-     * Unsubscribes from a Redis topic.
-     *
-     * @param topic - The topic to unsubscribe from.
-     */
-    unsubscribeFromTopic(topic) {
-        this.redisClient.unsubscribe(topic);
-    }
-    /**
-     * Starts a Redis transaction.
-     *
-     * @returns A Promise resolving when the transaction is started.
-     */
-    async startTransaction() {
-        await this.redisClient.multi();
     }
 }
 exports.RedisService = RedisService;
